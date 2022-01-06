69320ebddc88eb96cb0f7d45c1a1d79b7c0c9317
Daisho Komiyama <daishokomiyama@gmail.com>
2022-01-05 23:18:34 -0500

    Enable click to play

--- js/app.js
+++ js/app.js
@@ -1,34 +1,44 @@
-'use strict';
+'use strict'
 
 function playSound(event) {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-    stopSound(event);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add('playing');
+  const dataKey =
+    event.type === 'keydown' ? event.keyCode : event.target.dataset.key
+  const audio = document.querySelector(`audio[data-key="${dataKey}"]`)
+  const key = document.querySelector(`.key[data-key="${dataKey}"]`)
+  stopSound(event)
+  if (!audio) return
+
+  audio.currentTime = 0
+  audio.play()
+  key.classList.add('playing')
 }
 
 function stopSound(event) {
-    if (event.keyCode === 32) {
-        event.preventDefault();
-        audios.forEach(audio => audio.pause());
-    }
+  if (event.type === 'click' || event.keyCode === 32) {
+    event.preventDefault()
+    audios.forEach(audio => audio.pause())
+  }
 }
 
 function removeTransition(event) {
-    if (event.propertyName !== 'transform') return;
-    this.classList.remove('playing');
+  if (event.propertyName !== 'transform') return
+  this.classList.remove('playing')
 }
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 
-const audios = document.querySelectorAll('audio');
-audios.forEach(audio => audio.volume = .25);
+const audios = document.querySelectorAll('audio')
+audios.forEach(audio => (audio.volume = 0.25))
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playSound)
 
-const currentYear = new Date().getFullYear();
-document.getElementById("year").innerHTML = currentYear;
+const currentYear = new Date().getFullYear()
+document.getElementById('year').innerHTML = currentYear
+
+for (let i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('click', playSound)
+}
+
+const stop = document.getElementById('stop')
+stop.addEventListener('click', stopSound)
